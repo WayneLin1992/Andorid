2c3c8a1766bd2b27379ff9d626c87619ac23f6d9
WayneLin1992 <boy504052005@gmail.com>
2020-12-03 17:06:25 +0800

    First Push

---
+++ app/src/main/java/com/example/interactiondemo/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.interactiondemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public void clickFunction(View view){
+        Log.i( "Info", "button press" );
+
+
+    }
+    public void  clickNextPage(View view){
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this , Page2.class);
+        startActivity(intent);
+    }
+    public void login(View view){
+        EditText Username = (EditText) findViewById(R.id.editTextUsername );
+        EditText Password = (EditText) findViewById(R.id.editTextPassword );
+        Log.i(  "Info", "button pass");
+        Log.i(  "Username",Username.getText().toString());
+        Log.i(  "Password",Password.getText().toString());
+        Toast.makeText(this,   "hi " + Username.getText().toString(), Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
